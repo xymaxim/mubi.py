6192e6b0cce9ab4304ac54b3211f8fa363e4ae3c
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-11-17 22:12:17 +0400

    First commit

---
+++ test_mubi.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import os
+import unittest
+
+from mubi import Mubi, MubiException
+
+
+MUBI_EMAIL = os.getenv('MUBI_EMAIL')
+MUBI_PASSWORD = os.getenv('MUBI_PASSWORD')
+MUBI_USER_ID = os.getenv('MUBI_USER_ID')
+
+class MubiTest(unittest.TestCase):
+    def test_login(self):
+        m = Mubi(MUBI_EMAIL, MUBI_PASSWORD)
+        assert m.user_id == MUBI_USER_ID
+
+    def test_invalid_credentials(self):
+        with self.assertRaises(MubiException):
+            m = Mubi('invalid', 'credentials')
+
+
+if __name__ == '__main__':
+    unittest.main()
