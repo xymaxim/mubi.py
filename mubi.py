e42c88974b17b2bc93999795a358ccf921f3e901
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-11-17 22:22:12 +0400

    It works, make the test pass

---
+++ mubi.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+import requests
+from lxml.html import fromstring
+
+
+class MubiException(Exception):
+    pass
+
+
+class Mubi(object):
+    def __init__(self, email, password):
+        if not email or not password:
+            raise ValueError(
+                "Credentials must be specified. "
+                "Pass them into Mubi constructor"
+            )
+
+        self.s = requests.Session()
+
+        self.user_id = self._login(email, password)
+
+    def _login(self, email, password):
+        login_html = self.s.get('https://mubi.com/login').content
+        auth_token = (fromstring(login_html)
+                      .xpath("//input[@name='authenticity_token']/@value"))
+        payload = {
+            'utf8': '✓',
+            'authenticity_token': auth_token,
+            'email': email,
+            'password': password,
+            'x': 0,
+            'y': 0
+        }
+
+        response = self.s.post('https://mubi.com/session', data=payload)
+
+        # Once a user successfully authenticates, MUBI will redirect a user
+        # to the homepage, otherwise the login page will be shown.
+        if response.url == 'http://mubi.com/home':
+            # Yet another redirect to the user profile page, e.g. /users/12345
+            user_url = self.s.get('http://mubi.com/profile').url
+            user_id = user_url.split('/')[-1]
+            return user_id
+        elif response.url == 'https://mubi.com/login':
+            raise MubiException("Sorry, email or password doesn't work")
