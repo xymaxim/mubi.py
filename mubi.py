7c3f4f49e1b7a5277269fb2bc7651a6a9c2973fa
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-11-26 01:08:24 +0400

    Stop writing classes

--- mubi.py
+++ mubi.py
@@ -8,54 +8,43 @@ class MubiException(Exception):
     pass
 
 
-class Mubi(object):
-    def __init__(self, email, password):
-        if not email or not password:
-            raise ValueError(
-                "Credentials must be specified. "
-                "Pass them into Mubi constructor"
-            )
+def login(email, password, identify=False):
+    """Returns a logged in requests.Session object.
+    Enable `identify` to get a tuple in the form (session object, user id)
 
-        self.session = requests.Session()
-        self.me = self._login(email, password)
+    If given email address or password fails you get a `MubiException`.
+    """
+    if not email or not password:
+        raise ValueError("Credentials must be specified. "
+                         "Pass them into login method")
 
-    def _login(self, email, password):
-        login_html = self.session.get('https://mubi.com/login').text
+    session = requests.Session()
 
-        # Everybody stand back!
-        m = re.search('<input\s+name="authenticity_token".*?value="(.*?)"\s*\/>',
-                      login_html)
-        if m is None:
-            raise ValueError(
-                "Can't match authenticity token. "
-                "It seems the login page has changed."
-            )
+    login_html = session.get('https://mubi.com/login').text
 
-        auth_token = m.group(0) # our Achilles heel
-        payload = {
-            'utf8': '✓',
-            'authenticity_token': auth_token,
-            'email': email,
-            'password': password,
-            'x': 0,
-            'y': 0
-        }
+    # Everybody stand back!
+    m = re.search('<input\s+name="authenticity_token".*?value="(.*?)"\s*\/>',
+                  login_html)
+    if m is None:
+        raise ValueError("Unable to match authenticity token. "
+                         "It seems the login page has changed.")
 
-        response = self.session.post('https://mubi.com/session', data=payload)
-
-        # Once a user successfully authenticates, MUBI will redirect a user
-        # to the homepage, otherwise the login page will be shown.
-        if response.url == 'http://mubi.com/home':
-            # Yet another redirect to the user profile page, e.g. /users/12345
-            user_url = self.session.get('http://mubi.com/profile').url
+    response = session.post('https://mubi.com/session', data={
+        'utf8': '✓',
+        'authenticity_token': m.group(0),
+        'email': email,
+        'password': password,
+        'x': 0,
+        'y': 0
+    })
+    # Once a user successfully authenticates, MUBI will redirect a user
+    # to the homepage, otherwise the login page will be shown.
+    if response.url == 'http://mubi.com/home':
+        if identify:
+            # Yet another redirect to the user profile page, e.g. /users/123456
+            user_url = session.get('http://mubi.com/profile').url
             user_id = user_url.split('/')[-1]
-            return user_id
-        elif response.url == 'https://mubi.com/login':
-            raise MubiException("Sorry, email or password doesn't work")
-
-    def request(self, method, url_or_path, *args, **kwargs):
-        if '://' not in url_or_path:
-            url = 'http://mubi.com/' + url_or_path.lstrip('/')
-        else:
-            url = url_or_path
-        return self.session.request(method, url, *args, **kwargs)
+            return session, user_id
+        return session
+    elif response.url == 'https://mubi.com/login':
+        raise MubiException("Sorry, email or password doesn't work")
--- test_mubi.py
+++ test_mubi.py
@@ -3,7 +3,7 @@
 import os
 import unittest
 
-from mubi import Mubi, MubiException
+from mubi import login, MubiException
 
 
 MUBI_EMAIL = os.getenv('MUBI_EMAIL')
@@ -12,12 +12,12 @@ MUBI_USER_ID = os.getenv('MUBI_USER_ID')
 
 class MubiTest(unittest.TestCase):
     def test_login(self):
-        m = Mubi(MUBI_EMAIL, MUBI_PASSWORD)
-        assert m.me == MUBI_USER_ID
+        _, me = login(MUBI_EMAIL, MUBI_PASSWORD, identify=True)
+        assert me == MUBI_USER_ID
 
     def test_invalid_credentials(self):
         with self.assertRaises(MubiException):
-            m = Mubi('invalid', 'credentials')
+            login('invalid', 'credentials')
 
 
 if __name__ == '__main__':
