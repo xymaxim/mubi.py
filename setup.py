dc608d0af2d10d8270a493096f314abc450ffeb3
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2013-11-28 17:10:21 +0400

    Setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="mubi.py",
+    description="Helps you log in to mubi.com",
+    version="1.0",
+    author="Maxim Stolyarchuk",
+    author_email="maxim.stolyarchuk@gmail.com",
+    url="http://github.com/mstolyarchuk/mubi.py",
+    install_requires=['requests'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
