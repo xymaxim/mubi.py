bdeb95d0de66a8694b75416ec821fbbe880ca30f
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2014-01-22 14:40:33 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
 setup(
-    name="mubi.py",
+    name="mubi",
+    version="0.1.0",
     description="Helps you log in to mubi.com",
-    version="1.0",
+    long_description=__doc__,
+    url="https://github.com/mstolyarchuk/mubi.py",
     author="Maxim Stolyarchuk",
     author_email="maxim.stolyarchuk@gmail.com",
-    url="http://github.com/mstolyarchuk/mubi.py",
+    py_modules=['mubi'],
+    zip_safe=False,
+    include_package_data=True,
     install_requires=['requests'],
     classifiers=[
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP'
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
