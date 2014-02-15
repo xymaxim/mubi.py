ee7d9bef554278cfb86cb207d2f8618c627b17f9
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2014-02-15 19:36:17 +0400

    Bump to v0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="mubi",
-    version="0.1.0",
-    description="Helps you log in to mubi.com",
+    name='mubi',
+    version='0.2.0',
+    description='Helps you log in to mubi.com',
     long_description=__doc__,
-    url="https://github.com/mstolyarchuk/mubi.py",
-    author="Maxim Stolyarchuk",
-    author_email="maxim.stolyarchuk@gmail.com",
+    url='https://github.com/mstolyarchuk/mubi.py',
+    author='Maxim Stolyarchuk',
+    author_email='maxim.stolyarchuk@gmail.com',
     py_modules=['mubi'],
     zip_safe=False,
     include_package_data=True,
