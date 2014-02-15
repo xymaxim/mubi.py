d0e30833fd4476587b48c9561bef49410cfd3a46
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2014-02-15 19:30:43 +0400

    Use ssl endpoints by default

--- mubi.py
+++ mubi.py
@@ -9,7 +9,7 @@ class MubiException(Exception):
     pass
 
 
-def mubicom(path, use_ssl=False):
+def mubicom(path, use_ssl=True):
     """Returns a full url for the given path."""
     protocol = 'https' if use_ssl else 'http'
     return '{0}://mubi.com/{1}'.format(protocol, path.lstrip('/'))
@@ -31,7 +31,7 @@ def login(email=None, password=None, session=None, identify=False):
     if session is None:
         session = requests.Session()
 
-    login_html = session.get(mubicom('login', True)).text
+    login_html = session.get(mubicom('login')).text
 
     # Everybody stand back!
     m = re.search('<input\s+name="authenticity_token".*?value="(.*?)"\s*\/>',
@@ -40,7 +40,7 @@ def login(email=None, password=None, session=None, identify=False):
         raise ValueError("Unable to match authenticity token. "
                          "It seems the login page has changed.")
 
-    response = session.post(mubicom('session', True), data={
+    response = session.post(mubicom('session'), data={
         'utf8': '✓',
         'authenticity_token': m.group(1),
         'session[email]': email,
@@ -53,10 +53,10 @@ def login(email=None, password=None, session=None, identify=False):
     # to the "now showing" page, otherwise the login page will be shown.
     if response.url == mubicom('films/showing', True):
         if identify:
-            # Yet another redirect to the user profile page, e.g. /users/123456
+            # Yet another redirect to the user profile page, e.g. /users/123456.
             user_url = session.get(mubicom('profile')).url
             user_id = user_url.split('/')[-1]
             return session, user_id
         return session
-    elif response.url == mubicom('session', True):
+    elif response.url == mubicom('session'):
         raise MubiException("Sorry, email or password doesn't work")
